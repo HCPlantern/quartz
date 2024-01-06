be93afde1e274cc6395368e0877592fec80163ca
hcplantern <hcplantern@foxmail.com>
2024-01-06 14:54:20 +0800

    Updated: 2024-01-06 14:54:20

--- quartz/components/LinkesHeader.tsx
+++ quartz/components/LinkesHeader.tsx
@@ -15,10 +15,18 @@ export default (() => {
             <a href="https://hcplantern.top/">Blog</a>
           </span>
           <span>
-            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Books/Color/books_color.svg"></img>
-            <a href="/Sciujo/MAC/MAC">MAC</a>
+            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Laptop/Color/laptop_color.svg"></img>
+            <a href="/Comp">Computer</a>
           </span>
           <span>
+            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Books/Color/books_color.svg"></img>
+            <a href="/Books">Books</a>
+          </span>
+          <span>
+            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Books/Color/books_color.svg"></img>
+            <a href="/tags">Books</a>
+          </span>
+          {/* <span>
             <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Brain/Color/brain_color.svg"></img>
             <a href="/Sciujo/Sciujo">Learn</a>
           </span>
@@ -29,11 +37,7 @@ export default (() => {
           <span>
             <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Triangular%20ruler/Color/triangular_ruler_color.svg"></img>
             <a href="/Sciujo/Mathematics">Maths</a>
-          </span>
-          <span>
-            <img src="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Laptop/Color/laptop_color.svg"></img>
-            <a href="/Sciujo/Computing">Comp</a>
-          </span>
+          </span> */}
         </div>
       <hr style="background-color: var(--gray); border-top: 1px var(--gray) solid; margin-top: 1.3rem"></hr>
       </div>
